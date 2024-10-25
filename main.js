f77079acfec30680e706fcd73c3c7e080c4fc416
sandhyadhanenkula196 <sandhyadhanenkula2020@gmail.com>
2024-10-25 14:55:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//main.js
+
+ // Import createApp from Vue
+ import { createApp } from 'vue';
+ import App from './App.vue';
+ 
+ // Create a Vue app instance
+ const app = createApp(App);
+ 
+ // Mount the app to the DOM
+ app.mount('#app');
+ 
